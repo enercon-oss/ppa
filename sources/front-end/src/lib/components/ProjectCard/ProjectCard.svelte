cbfd19c4a58cb41b0a6ca0cf5f6f21b0e14dc0e7
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2023-11-04 20:27:16 +0100

    added project card

---
+++ sources/front-end/src/lib/components/ProjectCard/ProjectCard.svelte
@@ -0,0 +1,24 @@
+<script>
+  /**
+   * @type {string}
+   */
+  export let id;
+  /**
+   * @type {string}
+   */
+  export let name;
+</script>
+
+<style>
+  article {
+    background-color: var(--theme-black);
+    color: var(--theme-white);
+
+    min-height: 15rem;
+  }
+</style>
+
+<article>
+  <div>{id}</div>
+  <div>{name}</div>
+</article>
--- sources/front-end/src/lib/containers/Footer/Footer.svelte
+++ sources/front-end/src/lib/containers/Footer/Footer.svelte
@@ -3,6 +3,8 @@
     grid-area: site-footer;
     position: sticky;
     bottom: 0;
+    gap: 0.5rem;
+    padding: 0.25rem 0;
   }
 </style>
 <footer>
